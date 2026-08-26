dac8cf9eb2a91eb1809dd0073daa237ae0c81404
Logan Amos <lamos@binghamton.edu>
2026-08-26 16:35:32 -0400

    initial commit

---
+++ portfolio/README.md
@@ -0,0 +1,5 @@
+# CS110: Introduction to Programming Portfolio
+
+- Preferred Name: Logan Amos
+- Discord Username: goob4president
+- Github Username: logan-p-a
---
+++ portfolio/ch00/lab/main.py
@@ -0,0 +1,14 @@
+weeks = 16
+classes = 5
+tuition = 6000
+
+cost_per_week = (tuition / classes) / weeks
+print("Cost per week:", cost_per_week)
+
+classes_per_week = 3
+
+cost_per_class = cost_per_week / classes_per_week
+print(cost_per_class, type(cost_per_class)) 
+print(classes_per_week, type(classes_per_week)) 
+print(classes_per_week, type(classes_per_week))
+print(cost_per_class, "You got this!!")
