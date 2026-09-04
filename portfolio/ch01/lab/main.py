c7f6f8d695946737703938c68661bc1ea5c5e784
Logan Amos <lamos@binghamton.edu>
2026-09-04 08:22:03 -0400

    Complete Lab 1

--- portfolio/ch01/lab/main.py
+++ portfolio/ch01/lab/main.py
@@ -1,6 +1,17 @@
 # Part A
 weeks = 16
+print(weeks, " ", type(weeks))
 classes = 5
-tuition = 6000
+print(classes, " ", type(classes))
+tuition = 6000  
+print(tuition, " ", type(tuition))
 cost_per_week = (tuition / classes) / weeks
+print(cost_per_week, " ", type(cost_per_week)) 
 print("Cost per week:", cost_per_week)
+classes_per_week = 10
+print(classes_per_week, " ", type(classes_per_week))
+cost_per_week = cost_per_week / classes_per_week
+print(cost_per_week, " ", type(cost_per_week))
+cost_per_class = cost_per_week/classes_per_week
+print(cost_per_class, " ", type(cost_per_class))
+print("The cost per class is: ", cost_per_class)
