e0f074b52aac929e24243108a6ccee2a2e60cc00
Logan Amos <lamos@binghamton.edu>
2026-09-09 16:55:03 -0400

    Added code

---
+++ portfolio/Class_activity/my_test.py
@@ -0,0 +1,19 @@
+import turtle
+my_turtle = turtle.Turtle()
+window = turtle.Screen()
+window.title("My First Turtle Program")
+my_turtle.shape("turtle")
+my_turtle.color("blue")
+for i in range(4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+my_turtle.penup()
+my_turtle.goto(150, 0)
+my_turtle.pendown()
+for i in range(3):
+    my_turtle.forward(100)
+    my_turtle.right(120)
+for i in range(5, 10, 2):
+    print(i)
+print(list(range(2, 6)))
+window.exitonclick()
