60edec8f36ccd8235072a3da4d6762d53db2040c
Logan Amos <lamos@binghamton.edu>
2026-09-18 08:19:50 -0400

    Completed ch02 lab

---
+++ portfolio/Class_activity/boolean_practice.py
@@ -0,0 +1,9 @@
+a=2
+b=5
+c=3
+if a > b and a>c:
+    print("a is the greatest value")
+elif b>c:
+    print("b is the greatest value")
+else:
+    print("C is the greatest value")
--- portfolio/ch02/lab/main.py
+++ portfolio/ch02/lab/main.py
@@ -18,9 +18,48 @@ michelangelo.goto(-100, 20)
 leonardo.goto(-100, -20)
 
 ## 5. Your PART A code goes here
+m_distance = random.randint(1, 101)
+michelangelo.forward(m_distance)
+l_distance = random.randint(1, 101)
+leonardo.forward(l_distance)
 
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
 
+for i in range(10):
+    m_distance = random.randint(1, 11)
+    michelangelo.forward(m_distance)
+    l_distance = random.randint(1, 11)
+    leonardo.forward(l_distance)
+
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
 # PART B - complete part B here
-
+leonardo.down()
+for i in range(3):
+    leonardo.forward(50)
+    leonardo.left(360/3)
+leonardo.clear()
+for i in range(4):
+    leonardo.forward(50)
+    leonardo.left(360/4)
+leonardo.clear()
+for i in range(6):
+    leonardo.forward(50)
+    leonardo.left(360/6)
+leonardo.clear()
+for i in range(20):
+    leonardo.forward(50)
+    leonardo.left(360/20)
+leonardo.clear()
+for i in range(100):
+    leonardo.forward(10)
+    leonardo.left(360/100)
+leonardo.clear()
+leonardo.speed(0)
+for i in range(360):
+    leonardo.forward(2)
+    leonardo.left(360/360)
+leonardo.clear()
 
 window.exitonclick()
